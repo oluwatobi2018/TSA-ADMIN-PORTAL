2a9047d79c92dc7e9d265d20e9bdec8bfad34fe1
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-29 08:40:58 +0100

    Update dahboard.blade.php

---
+++ resources/views/dahboard.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Admin Dashboard</h1>
+    
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Manage Users</h5>
+                    <p class="card-text">Create, edit, or delete user accounts.</p>
+                    <a href="{{route('users.index') }}" class="btn btn-primary">Go to Users</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Manage Contacts</h5>
+                    <p class="card-text">Add, update, or remove contacts.</p>
+                    <a href="{{route('contacts.index') }}" class="btn btn-primary">Go to Contacts</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Logout</h5>
+                    <p class="card-text">Log out of the admin panel.</p>
+                    <form action="{{route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Logout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
