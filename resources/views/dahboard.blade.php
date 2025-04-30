c4cb7dcfd3c365f263536d7d04d73a9386af0ff0
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 19:10:46 +0100

    Update dahboard.blade.php

--- resources/views/dahboard.blade.php
+++ resources/views/dahboard.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="container">
-    <h1>Admin Dashboard</h1>
+    <h1>Admin Portal</h1>
     
     <div class="row">
         <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Manage Users</h5>
+                    <h5 class="card-title">Dashboard</h5>
                     <p class="card-text">Create, edit, or delete user accounts.</p>
                     <a href="{{route('users.index') }}" class="btn btn-primary">Go to Users</a>
                 </div>
@@ -18,7 +18,7 @@
         <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Manage Contacts</h5>
+                    <h5 class="card-title">Contacts</h5>
                     <p class="card-text">Add, update, or remove contacts.</p>
                     <a href="{{route('contacts.index') }}" class="btn btn-primary">Go to Contacts</a>
                 </div>
